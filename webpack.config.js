6d55d7d70736c741c87ec2a0912b3c91bceef3e1
SirNeo <lepeonline@gmail.com>
2018-02-09 18:33:17 +0100

    Añadido ESlint

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,12 @@ module.exports = {
     module: {
         rules: [
             {
+               test: /\.js$/,
+               exclude: /node_modules/,
+               enforce: 'pre',
+               loader: 'eslint-loader',
+            },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
